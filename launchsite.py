0cf5173368c251f84bdc67a514e1f54867dc4b87
Owen Shaule <55895503+Ow1e@users.noreply.github.com>
2022-02-27 11:28:47 -0500

    Fix launchsite

--- launchsite.py
+++ launchsite.py
@@ -1,7 +1,11 @@
 from openloop.virtualizer import Database
+import os
 
 db = Database()
 db.restore()
+
+if os.path.exists("errors.log"):
+    os.remove("errors.log")
 
 # Alert System
 from openloop.alerts import AlertManager
@@ -11,5 +15,13 @@ print("Runing plugin emulation...")
 # Enable Plugins and Workers
 from openloop.workers import WorkerHandler
 worker_handle = WorkerHandler(db, alerts)
+
+if os.path.exists("errors.log"):
+    with open("errors.log") as f:
+        data = f.read().split("\n")
+    for i in data:
+        print(i)
+else:
+    print("No errors compiling...")
 
 print("Launch site completed... feel free to exit now")
